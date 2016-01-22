fad9904cbcb8ddf93cfbb11e5a3500837e3b3003
Eli Davis <eli.davis1995@gmail.com>
2016-01-22 14:36:55 -0600

    Created Example Game Mode
    Really small structure of what creating a game mode would be like,
    explain how events are sent across the program.
    
    NEED TO ADD GAME MANAGER

---
+++ Assets/Scripts/Character/CharacterBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VGDC.Character {
+
+	public class CharacterBehavior : MonoBehaviour {
+
+		private string name;
+
+		public string getName(){
+			return name;
+		}
+
+		void Start(){
+			name = "";
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/GameManagement/GameMode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using VGDC.Character;
+
+namespace VGDC.GameManagement {
+
+	public interface GameMode {
+
+		void onGameStart();
+
+		void onCharacterDeath(CharacterBehavior character);
+
+	}
+
+}
